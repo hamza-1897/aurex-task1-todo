e7a843f0303f1e1301a71da43ebc0abe984c18b3
Muhammad Hamza <hanjaa1897@gmail.com>
2026-04-05 21:51:55 +0500

    feat: state management and dynamic list rendering

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,15 +1,18 @@
 import { View, TextInput, TouchableOpacity, Text } from 'react-native';
 
-const InputArea = () => {
+const InputArea = ({ task, setTask, onAdd }) => {
   return (
     <View className="flex-row p-4 items-center space-x-2">
       <TextInput 
         className="flex-1 bg-gray-100 border border-gray-300 rounded-lg px-4 py-3 text-base"
         placeholder="Add a new task..."
         placeholderTextColor="#9ca3af"
+        value={task}
+        onChangeText={(text) => setTask(text)}
       />
       <TouchableOpacity 
         className="bg-blue-600 px-6 py-3 rounded-lg active:bg-blue-700"
+        onPress={onAdd}
       >
         <Text className="text-white font-semibold text-base">Add</Text>
       </TouchableOpacity>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text,FlatList } from 'react-native'
+import { useState } from 'react';
 import { StatusBar } from "expo-status-bar";
 import Header from "../components/Header";
 import InputArea from '../components/Input';
 
 const Home = () => {
+
+  //States initialize for task input and task list
+  const [task, setTask] = useState(''); 
+  const [taskList, setTaskList] = useState([]); 
+
+
+  //Add Task Function
+  const handleAddTask = () => {
+    if (task.trim().length === 0) return;
+
+    const newTask = {
+      id: Date.now().toString(), 
+      text: task,
+      completed: false,
+    };
+
+    setTaskList([...taskList, newTask]); 
+    setTask(''); 
+  };
   return (
     <View>
         <Header />
-        <InputArea />
+        <InputArea 
+        task={task} 
+        setTask={setTask} 
+        onAdd={handleAddTask}
+        />
+
+        <FlatList
+        data={taskList}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View className="bg-gray-100 p-4 mb-2 rounded-lg shadow-sm">
+            
+            <Text className="text-gray-800 text-lg">{item.text}</Text>
+          
+          </View>
+        )}
+      />
         <StatusBar style="auto" />
 
     </View>
